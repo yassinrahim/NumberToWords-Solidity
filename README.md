58bdb99452aa5fefbaa76cb13cc281b460bd750d
yassinrahim <yassin_rahim93@yahoo.com>
2023-01-17 23:20:37 +0800

    1. Added license 2. Added ReadMe 3. Cleaned NumberToWords.sol

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# NumberToWord Solidity Library
+# Number To Word Solidity Library
 
+This library was made by refractoring the following number-to-words repository:
+https://github.com/marlun78/number-to-words
+
+Contains some util methods for converting numbers into words, ordinal words and ordinal numbers.
+
+## Install
+`npm install numbertowords-solidity`
+
+## Usage
+
+### API
+#### `toWords(number)`
+Converts an integer into words.
+If number is decimal, the decimals will be removed.
+```solidity
+import "./NumberToWords.sol";
+
+NumberToWords.toWords(13); // => “thirteen”
+
+NumberToWords.toWords(56); // => “fifty-six”
+
+// Large numbers:
+NumberToWords.toWords(9007199254740992); // => “nine quadrillion, seven trillion, one hundred ninety-nine billion, two hundred fifty-four million, seven hundred forty thousand, nine hundred ninety-two”
+```
+
+## Acknowledgments
+* [marlun78/number-to-words](https://github.com/marlun78/number-to-words)
+
+## Thank You
+Feel free to contact me at
+
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://www.linkedin.com/in/muhammad-yassin-abd-rahim-057683109/
--- contracts/NumberToWords.sol
+++ contracts/NumberToWords.sol
@@ -3,115 +3,12 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./Strings.sol";
-import "hardhat/console.sol";
 
 // This library converts numbers to words in English
 
 library NumberToWords {
     using SafeMath for uint;
     using Strings for string;
-
-    // // Constants for the names of the digits
-    // string private constant DIGIT_NAMES = "zero,one,two,three,four,five,six,seven,eight,nine";
-    // // Constants for the names of the tens
-    // string private constant TENS_NAMES =
-    //     "ten,twenty,thirty,forty,fifty,sixty,seventy,eighty,ninety";
-    // // Constants for the names of the teens
-    // string private constant TEEN_NAMES =
-    //     "eleven,twelve,thirteen,fourteen,fifteen,sixteen,seventeen,eighteen,nineteen";
-    // // Constant for the max number of digits that the library can convert
-    // uint private constant MAX_DIGITS = 15;
-
-    // // Converts a number to its word representation
-    // function toWords(uint256 _number) public view returns (string memory) {
-    //     if (_number > 10 ** MAX_DIGITS) {
-    //         // Return an empty string if the number is too large
-    //         return "";
-    //     }
-    //     if (_number == 0) {
-    //         // Return "zero" for 0
-    //         return "zero";
-    //     }
-    //     string memory result = "";
-    //     uint num = _number;
-    //     uint[] memory digits = new uint[](MAX_DIGITS);
-    //     uint numDigits = 0;
-    //     // Split the number into its digits
-    //     while (num > 0) {
-    //         digits[numDigits] = num % 10;
-    //         num = num / 10;
-    //         numDigits++;
-    //     }
-
-    //     // Convert the digits to words, starting with the highest place value
-    //     for (uint j = numDigits; j > 0; j--) {
-    //         uint i = j - 1;
-    //         uint digit = digits[i];
-
-    //         if (i == 9) {
-    //             // Billion
-    //             if (digit > 0) {
-    //                 result = result.concat(convertDigit(digit)).concat(" billion ");
-    //             }
-    //         } else if (i == 6) {
-    //             // Million
-    //             if (digit > 0) {
-    //                 result = result.concat(convertDigit(digit)).concat(" million ");
-    //             }
-    //         } else if (i == 3) {
-    //             // Thousand
-    //             if (digit > 0) {
-    //                 result = result.concat(convertDigit(digit)).concat(" thousand ");
-    //             }
-    //         } else if (i == 2) {
-    //             // Hundred
-    //             if (digit > 0) {
-    //                 result = result.concat(convertDigit(digit)).concat(" hundred ");
-    //             }
-    //         } else if (i == 1) {
-    //             // Ten
-    //             if (digit > 1) {
-    //                 result = result.concat(convertTens(digit));
-    //             } else if (digit == 1) {
-    //                 uint firstDigit = digits[0];
-    //                 if (firstDigit == 0) {
-    //                     result = result.concat(convertTens(digit));
-    //                 } else {
-    //                     result = result.concat(convertTeens(digits[0]));
-    //                 }
-    //                 break;
-    //             }
-    //         } else if (i == 0) {
-    //             // One
-    //             if (digit > 0) {
-    //                 if (result.length() != 0) {
-    //                     result = result.concat(" ");
-    //                 }
-    //                 result = result.concat(convertDigit(digit));
-    //             }
-    //         }
-    //     }
-
-    //     return result;
-    // }
-
-    // // Converts a single digit to its word representation
-    // function convertDigit(uint _digit) private pure returns (string memory) {
-    //     string[] memory digitNames = DIGIT_NAMES.split(",");
-    //     return digitNames[_digit];
-    // }
-
-    // // Converts a number in the tens place to its word representation
-    // function convertTens(uint _tens) private pure returns (string memory) {
-    //     string[] memory tensNames = TENS_NAMES.split(",");
-    //     return tensNames[_tens - 1];
-    // }
-
-    // // Converts a number in the teens to its word representation
-    // function convertTeens(uint _teen) private pure returns (string memory) {
-    //     string[] memory teenNames = TEEN_NAMES.split(",");
-    //     return teenNames[_teen - 1];
-    // }
 
     uint private constant TEN = 10;
     uint private constant ONE_HUNDRED = 100;
